819d877091b2007b9b724f76865061261e4832c8
Taylor Girard <taylorgirard@fb.com>
2022-06-20 15:20:55 -0700

    Bottom Navigation view created; user can go between their profile and matches

--- app/src/main/java/com/taylorgirard/comicconvo/activities/MainActivity.java
+++ app/src/main/java/com/taylorgirard/comicconvo/activities/MainActivity.java
@@ -1,17 +1,49 @@
 package com.taylorgirard.comicconvo.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.taylorgirard.comicconvo.R;
-
+import com.taylorgirard.comicconvo.fragments.MatchesFragment;
+import com.taylorgirard.comicconvo.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        fragment = new ProfileFragment();
+                        break;
+                    case R.id.action_matches:
+                        fragment = new MatchesFragment();
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        //Set default
+        bottomNavigationView.setSelectedItemId(R.id.action_profile);
     }
 }
