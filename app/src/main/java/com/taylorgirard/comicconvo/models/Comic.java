6263662bdc590853af801435e21ad917acaa8a75
Taylor Girard <taylorgirard@fb.com>
2022-06-22 13:54:52 -0700

    Comic model created https://github.com/Taylor-Girard/ComicConvo/issues/4

---
+++ app/src/main/java/com/taylorgirard/comicconvo/models/Comic.java
@@ -0,0 +1,45 @@
+package com.taylorgirard.comicconvo.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Comic {
+
+    String coverPath;
+    String title;
+
+    public Comic(JSONObject jsonObject) throws JSONException {
+
+        JSONObject images = jsonObject.getJSONObject("images");
+        coverPath = images.getString("path") + "." + images.getString("extension");
+        title = jsonObject.getString("title");
+
+    }
+
+    public static List<Comic> fromJsonArray(JSONArray comicJsonArray) throws JSONException {
+
+        List<Comic> comics = new ArrayList<>();
+
+        for (int i = 0; i < comicJsonArray.length(); i++){
+            comics.add(new Comic(comicJsonArray.getJSONObject(i)));
+        }
+
+        return comics;
+
+    }
+
+    public String getCoverPath(){
+        return coverPath;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+
+
+}
