4a127a0b44f51ae488ba563f587fda934810cf5d
Taylor Girard <taylorgirard@fb.com>
2022-06-22 16:25:52 -0700

    List of comics on search page gets updated depending on search query https://github.com/Taylor-Girard/ComicConvo/issues/4

--- app/src/main/java/com/taylorgirard/comicconvo/activities/ComicSearchActivity.java
+++ app/src/main/java/com/taylorgirard/comicconvo/activities/ComicSearchActivity.java
@@ -7,8 +7,11 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.RequestParams;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.taylorgirard.comicconvo.R;
 import com.taylorgirard.comicconvo.adapters.ComicAdapter;
@@ -22,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
+import okhttp3.Request;
 
 public class ComicSearchActivity extends AppCompatActivity {
 
@@ -29,12 +33,14 @@ public class ComicSearchActivity extends AppCompatActivity {
     public static final String BASE_COMIC_URL = "https://gateway.marvel.com/v1/public/comics?ts=123&apikey=865d88b76772d9a6a5fadb1f268538d4&hash=70025aec317dbd24c106b1867b239b36";
 
     List<Comic> comics;
+    SearchView svComicSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic_search);
         RecyclerView rvComicList = findViewById(R.id.rvComicList);
+        svComicSearch = findViewById(R.id.svComicSearch);
         comics = new ArrayList<>();
 
         final ComicAdapter comicAdapter = new ComicAdapter(this, comics);
@@ -61,6 +67,43 @@ public class ComicSearchActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
                 Log.e(TAG, "onFailure " + statusCode);
+            }
+        });
+
+        svComicSearch.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(ComicSearchActivity.this, query, Toast.LENGTH_SHORT).show();
+                RequestParams params = new RequestParams();
+                params.put("titleStartsWith", query);
+                client.get(BASE_COMIC_URL, params, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.d(TAG,"onSuccess");
+                        JSONObject jsonObject = json.jsonObject;
+                        try {
+                            JSONObject data = jsonObject.getJSONObject("data");
+                            JSONArray results = data.getJSONArray("results");
+                            Log.i(TAG, "Results" + results.toString());
+                            comics.clear();
+                            comics.addAll(Comic.fromJsonArray(results));
+                            comicAdapter.notifyDataSetChanged();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure " + statusCode);
+                    }
+                });
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
             }
         });
     }
