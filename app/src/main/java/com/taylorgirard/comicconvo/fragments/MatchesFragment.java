c64b878e544f5facda6115b95d94d8932aab76db
Taylor Girard <taylorgirard@fb.com>
2022-06-24 11:59:23 -0700

    Message button goes to empty message activity; will connect to users and design later on https://github.com/Taylor-Girard/ComicConvo/issues/5

--- app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
+++ app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
@@ -1,17 +1,25 @@
 package com.taylorgirard.comicconvo.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.taylorgirard.comicconvo.R;
+import com.taylorgirard.comicconvo.activities.IndividualMessageActivity;
 
 
 public class MatchesFragment extends Fragment {
+
+    ImageButton ibMessage;
 
     public MatchesFragment(){
         //empty constructor
@@ -22,5 +30,20 @@ public class MatchesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_matches, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        ibMessage = view.findViewById(R.id.ibMessage);
+
+        ibMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), IndividualMessageActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
