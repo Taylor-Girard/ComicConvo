98bb0dc82077572e014c4b6be732d80cedac5dce
Taylor Girard <taylorgirard@fb.com>
2022-06-28 11:48:14 -0700

    New match information is loaded when the user hits the skip button https://github.com/Taylor-Girard/ComicConvo/issues/5

--- app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
+++ app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
@@ -1,6 +1,7 @@
 package com.taylorgirard.comicconvo.fragments;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,6 +24,7 @@ import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.taylorgirard.comicconvo.R;
 import com.taylorgirard.comicconvo.activities.IndividualMessageActivity;
+import com.taylorgirard.comicconvo.activities.MainActivity;
 import com.taylorgirard.comicconvo.adapters.MatchListAdapter;
 import com.taylorgirard.comicconvo.adapters.UserListAdapter;
 import com.taylorgirard.comicconvo.models.Comic;
@@ -37,6 +40,7 @@ public class MatchesFragment extends Fragment {
     public static final int LIST_COLUMNS = 2;
 
     ImageButton ibMessage;
+    ImageButton ibSkip;
     ImageView ivMatchProfile;
     TextView tvAboutMatch;
     RecyclerView rvMatchLikes;
@@ -62,7 +66,6 @@ public class MatchesFragment extends Fragment {
         ParseUser currentUser = ParseUser.getCurrentUser();
         ParseUser match = currentUser.getParseUser("bestMatch");
 
-
 //
 //        try {
 //            Match.findMatch(currentUser);
@@ -71,10 +74,84 @@ public class MatchesFragment extends Fragment {
 //        }
 
         ibMessage = view.findViewById(R.id.ibMessage);
+        ibSkip = view.findViewById(R.id.ibSkip);
         ivMatchProfile = view.findViewById(R.id.ivMatchProfile);
         tvAboutMatch = view.findViewById(R.id.tvAboutMatch);
         rvMatchLikes = view.findViewById(R.id.rvMatchLikes);
         rvMatchDislikes = view.findViewById(R.id.rvMatchDislikes);
+
+        loadInfo(match);
+
+//        List<Comic> matchDislikes = null;
+//        try {
+//            matchDislikes = match.fetchIfNeeded().getList("Dislikes");
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        dislikes = new ArrayList<Comic>();
+//        try {
+//            dislikes.addAll(Comic.fromParseArray(matchDislikes));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        final MatchListAdapter comicAdapterDislikes = new MatchListAdapter(getContext(), dislikes);
+//        rvMatchDislikes.setAdapter(comicAdapterDislikes);
+//        rvMatchDislikes.setLayoutManager(new GridLayoutManager(getContext(), LIST_COLUMNS));
+//
+//        List<Comic> matchLikes= null;
+//        try {
+//            matchLikes = match.fetchIfNeeded().getList("Likes");
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        likes = new ArrayList<Comic>();
+//        try {
+//            likes.addAll(Comic.fromParseArray(matchLikes));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        final MatchListAdapter comicAdapterLikes = new MatchListAdapter(getContext(), likes);
+//        rvMatchLikes.setAdapter(comicAdapterLikes);
+//        rvMatchLikes.setLayoutManager(new GridLayoutManager(getContext(), LIST_COLUMNS));
+//
+//        try {
+//            tvAboutMatch.setText(match.fetchIfNeeded().getString("aboutMe"));
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
+//
+//        ParseFile matchPic = match.getParseFile("profilePic");
+//        if (matchPic != null) {
+//            Glide.with(getContext()).load(matchPic.getUrl()).transform(new CircleCrop()).into(ivMatchProfile);
+//        }
+
+        ibMessage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), IndividualMessageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ibSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Match.findMatch(currentUser);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                ParseUser newMatch = currentUser.getParseUser("bestMatch");
+                loadInfo(newMatch);
+            }
+        });
+    }
+
+    public void loadInfo(ParseUser match){
 
         List<Comic> matchDislikes = null;
         try {
@@ -123,12 +200,5 @@ public class MatchesFragment extends Fragment {
             Glide.with(getContext()).load(matchPic.getUrl()).transform(new CircleCrop()).into(ivMatchProfile);
         }
 
-        ibMessage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getContext(), IndividualMessageActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
