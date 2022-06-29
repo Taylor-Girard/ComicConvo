5abeda640c6a90210f0d4af7900d5da947412223
Taylor Girard <taylorgirard@fb.com>
2022-06-29 15:04:10 -0700

    Old user information has animation swiping off of screen when user hits skip https://github.com/Taylor-Girard/ComicConvo/issues/5

--- app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
+++ app/src/main/java/com/taylorgirard/comicconvo/fragments/MatchesFragment.java
@@ -1,20 +1,21 @@
 package com.taylorgirard.comicconvo.fragments;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -26,9 +27,7 @@ import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.taylorgirard.comicconvo.R;
 import com.taylorgirard.comicconvo.activities.IndividualMessageActivity;
-import com.taylorgirard.comicconvo.activities.MainActivity;
 import com.taylorgirard.comicconvo.adapters.MatchListAdapter;
-import com.taylorgirard.comicconvo.adapters.UserListAdapter;
 import com.taylorgirard.comicconvo.models.Comic;
 import com.taylorgirard.comicconvo.tools.ListType;
 import com.taylorgirard.comicconvo.tools.Match;
@@ -42,6 +41,7 @@ public class MatchesFragment extends Fragment {
     public static final String TAG = "Matches Fragment";
     public static final int LIST_COLUMNS = 2;
 
+    ConstraintLayout clMatchLayout;
     TextView tvMatchUsername;
     ImageButton ibMessage;
     ImageButton ibSkip;
@@ -88,6 +88,7 @@ public class MatchesFragment extends Fragment {
         }
         ParseUser match = currentUser.getParseUser("bestMatch");
 
+        clMatchLayout = view.findViewById(R.id.clMatchLayout);
         tvMatchUsername = view.findViewById(R.id.tvMatchUsername);
         ibMessage = view.findViewById(R.id.ibMessage);
         ibSkip = view.findViewById(R.id.ibSkip);
@@ -109,6 +110,8 @@ public class MatchesFragment extends Fragment {
         ibSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.slide_out_animation);
+                clMatchLayout.startAnimation(animation);
                 try {
                     Match.findMatch(currentUser);
                 } catch (ParseException e) {
