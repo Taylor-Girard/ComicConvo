fd360cc3b62363024bfc8364e4ebd3c3c51d87bc
Taylor Girard <taylorgirard@fb.com>
2022-07-08 16:16:19 -0700

    Message page displays messages from everyone; need to refine to only user and match https://github.com/Taylor-Girard/ComicConvo/issues/8

--- app/src/main/java/com/taylorgirard/comicconvo/activities/IndividualMessageActivity.java
+++ app/src/main/java/com/taylorgirard/comicconvo/activities/IndividualMessageActivity.java
@@ -1,6 +1,8 @@
 package com.taylorgirard.comicconvo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.location.Location;
@@ -9,34 +11,54 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.parse.FindCallback;
 import com.parse.ParseException;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.taylorgirard.comicconvo.R;
+import com.taylorgirard.comicconvo.adapters.MessageAdapter;
 import com.taylorgirard.comicconvo.models.Message;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class IndividualMessageActivity extends AppCompatActivity {
 
     public static final String TAG = "IndividualMessageActivity";
+    static final int MAX_CHAT_MESSAGES_TO_SHOW = 50;
 
-    TextView tvSampleText;
     EditText etMessage;
     ImageButton ibSend;
+    RecyclerView rvMessages;
+    ArrayList<Message> messages;
+    MessageAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_message);
+        setupMessagePosting();
+        loadMessages();
+    }
 
-        tvSampleText = findViewById(R.id.tvSampleText);
+    void setupMessagePosting(){
         etMessage = findViewById(R.id.etMessage);
         ibSend = findViewById(R.id.ibSend);
+        rvMessages = findViewById(R.id.rvMessages);
+        messages = new ArrayList<>();
 
         ParseUser match = getIntent().getParcelableExtra("Match");
 
-        tvSampleText.setText(match.getUsername());
+        adapter = new MessageAdapter(IndividualMessageActivity.this, match, messages);
+        rvMessages.setAdapter(adapter);
+
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(IndividualMessageActivity.this);
+        linearLayoutManager.setReverseLayout(true);
+        rvMessages.setLayoutManager(linearLayoutManager);
 
         ibSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,4 +82,31 @@ public class IndividualMessageActivity extends AppCompatActivity {
             }
         });
     }
+
+    void loadMessages(){
+        ParseQuery<Message> query = ParseQuery.getQuery(Message.class);
+        // Configure limit and sort order
+        query.setLimit(MAX_CHAT_MESSAGES_TO_SHOW);
+
+        // get the latest 50 messages, order will show up newest to oldest of this group
+        query.orderByDescending("createdAt");
+        // Execute query to fetch all messages from Parse asynchronously
+        // This is equivalent to a SELECT query with SQL
+        query.findInBackground(new FindCallback<Message>() {
+            @Override
+            public void done(List<Message> newMessages, ParseException e) {
+                if (e == null) {
+                    messages.clear();
+                    messages.addAll(newMessages);
+                    adapter.notifyDataSetChanged(); // update adapter
+                    // Scroll to the bottom of the list on initial load
+                    rvMessages.scrollToPosition(0);
+                } else {
+                    Log.e("message", "Error Loading Messages" + e);
+                }
+            }
+
+        });
+    }
+
 }
--- app/src/main/java/com/taylorgirard/comicconvo/models/Message.java
+++ app/src/main/java/com/taylorgirard/comicconvo/models/Message.java
@@ -2,6 +2,7 @@ package com.taylorgirard.comicconvo.models;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 @ParseClassName("Message")
 public class Message extends ParseObject {
@@ -9,5 +10,26 @@ public class Message extends ParseObject {
     public Message(){
         //empty constructor
     }
+
+    public ParseUser getSender(){
+        return getParseUser("Sender");
+    }
+
+    public String getSenderId(){
+        return getSender().getObjectId();
+    }
+
+    public ParseUser getReceiver(){
+        return getParseUser("Receiver");
+    }
+
+    public String getReceiverId(){
+        return getReceiver().getObjectId();
+    }
+
+    public String getBody(){
+        return  getString("Body");
+    }
+
 
 }
