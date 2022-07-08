d13889d3cb2dd057f6ad13d676d1c1bff18e3dac
Taylor Girard <taylorgirard@fb.com>
2022-07-08 11:07:28 -0700

    User can send messages that are then saved in parse with the proper sender and receiver pointers https://github.com/Taylor-Girard/ComicConvo/issues/8

--- app/src/main/java/com/taylorgirard/comicconvo/activities/IndividualMessageActivity.java
+++ app/src/main/java/com/taylorgirard/comicconvo/activities/IndividualMessageActivity.java
@@ -2,16 +2,62 @@ package com.taylorgirard.comicconvo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 import com.taylorgirard.comicconvo.R;
+import com.taylorgirard.comicconvo.models.Message;
 
 public class IndividualMessageActivity extends AppCompatActivity {
+
+    public static final String TAG = "IndividualMessageActivity";
+
+    TextView tvSampleText;
+    EditText etMessage;
+    ImageButton ibSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_message);
+
+        tvSampleText = findViewById(R.id.tvSampleText);
+        etMessage = findViewById(R.id.etMessage);
+        ibSend = findViewById(R.id.ibSend);
+
+        ParseUser match = getIntent().getParcelableExtra("Match");
+
+        tvSampleText.setText(match.getUsername());
+
+        ibSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String body = etMessage.getText().toString();
+                Message message = new Message();
+                message.put("Body", body);
+                message.put("Receiver", match);
+                message.put("Sender", ParseUser.getCurrentUser());
+                message.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null){
+                            Log.e(TAG, "Error saving message", e);
+                        } else{
+                            Log.d(TAG, "Successfully saved message");
+                        }
+                    }
+                });
+                etMessage.setText("");
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/taylorgirard/comicconvo/models/Message.java
@@ -0,0 +1,13 @@
+package com.taylorgirard.comicconvo.models;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+@ParseClassName("Message")
+public class Message extends ParseObject {
+
+    public Message(){
+        //empty constructor
+    }
+
+}
