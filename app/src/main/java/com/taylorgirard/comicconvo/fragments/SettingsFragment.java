7a43390e186ae0cf445ac22c7747df4c15e95bfb
Taylor Girard <taylorgirard@fb.com>
2022-07-14 15:05:24 -0700

    User can change their desired do not disturb on/off times, which is saved in Parse in UTC time https://github.com/Taylor-Girard/ComicConvo/issues/9

--- app/src/main/java/com/taylorgirard/comicconvo/fragments/SettingsFragment.java
+++ app/src/main/java/com/taylorgirard/comicconvo/fragments/SettingsFragment.java
@@ -6,23 +6,40 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
+import android.widget.Toast;
 
 import com.parse.ParseException;
+import com.parse.ParseInstallation;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.taylorgirard.comicconvo.R;
+import com.taylorgirard.comicconvo.tools.TimeUtility;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.DateTimeException;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class SettingsFragment extends Fragment {
 
     public static final String TAG = "SettingsFragment";
 
     Switch swNotifications;
+    Button btnChangeTime;
+    EditText etTimeStart;
+    EditText etTimeEnd;
     ParseUser user;
 
     public SettingsFragment() {
@@ -40,8 +57,51 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         swNotifications = view.findViewById(R.id.swNotifications);
+        btnChangeTime = view.findViewById(R.id.btnChangeTime);
+        etTimeStart = view.findViewById(R.id.etTimeStart);
+        etTimeEnd = view.findViewById(R.id.etTimeEnd);
 
         user = ParseUser.getCurrentUser();
+
+        if (user.getNumber("StartDND") != null && user.getNumber("EndDND") != null){
+            int timeStart = user.getNumber("StartDND").intValue();
+            int timeEnd = user.getNumber("EndDND").intValue();
+            etTimeStart.setText(Integer.toString(TimeUtility.UTCtoDevice(timeStart)));
+            etTimeEnd.setText(Integer.toString(TimeUtility.UTCtoDevice(timeEnd)));
+        }
+
+        btnChangeTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int startTime = Integer.parseInt(etTimeStart.getText().toString());
+                    int endTime = Integer.parseInt(etTimeEnd.getText().toString());
+                    if (startTime < 0 || endTime > 24 || startTime > endTime){
+                        throw new Exception();
+                    }
+
+                    int intStart = TimeUtility.deviceToUTC(startTime);
+                    int intEnd = TimeUtility.deviceToUTC(endTime);
+
+                    user.put("StartDND", intStart);
+                    user.put("EndDND", intEnd);
+                    user.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null){
+                                Log.i(TAG, "Successfully saved DND times");
+                            } else {
+                                Log.e(TAG, "Error saving DND times", e);
+                            }
+                        }
+                    });
+
+                } catch(Exception e){
+                    Toast.makeText(getContext(), "Enter valid times (24 hour format)", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
         Boolean notifications = null;
         try {
             notifications = user.fetchIfNeeded().getBoolean("Notifications");
